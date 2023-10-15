b2a686b0e109a8d508961974d79d09ec016d262f
MaTuanKhoa <147571785+MaTuanKhoa@users.noreply.github.com>
2023-10-16 00:17:00 +0700

    Add files via upload

---
+++ bài 8.cpp
@@ -0,0 +1,19 @@
+//libary
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+	int count =0;
+	int a,n;
+
+
+	cout <<"nhap n: ";
+	cin>>n;
+	for (a=0;a<n;a++){
+		cout<<" "<<a;
+			}
+		
+	return 0;
+}
+	
